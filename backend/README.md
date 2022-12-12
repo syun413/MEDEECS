73a4835cf1e0363e124153fe8885bb4aa1a2c28f
shengyenlin <shengyenlin0501@gmail.com>
2022-12-12 04:07:28 +0000

    modify demo.py

--- backend/README.md
+++ backend/README.md
@@ -5,3 +5,10 @@
 ```
 conda env create -f env_demo.yml
 ```
+
+## Before running
+
+run demo
+```
+python3 demo.py
+```
--- backend/demo.py
+++ backend/demo.py
@@ -75,40 +75,65 @@ def main(args):
     model = model.to(device)
     model.eval()
 
-
     softmax = torch.nn.Softmax()
-    for data_idx in test_dl:
-        if data_idx.item() == 1:
-            text = test['TEXT'][data_idx.item()]
-            sents_idx, ab_locs, labels = test_data[data_idx]
 
-            print("Text:", text)
-            print("="*50)    
+    input_text = ['This is a demo text AB']
+    loc = 5
+    word = input_text[0].split(' ')[loc]
+    sents_idx = tokenizer.batch_encode_plus(
+                input_text, max_length=512, 
+                pad_to_max_length=True
+                )['input_ids']
+    ab_loc = torch.tensor([loc])
+    logits = model(sents_idx, ab_loc)
+    
+    prob = softmax(logits)
+    pred_5_value, pred_5_idx = torch.topk(prob, k=5, dim=1)
+    pred_words, pred_probs = [], []
+    pred_5_idx, pred_5_value =  pred_5_idx.cpu().detach().numpy()[0], pred_5_value.cpu().detach().numpy()[0]
+    for pred_idx, pred_value in zip(pred_5_idx, pred_5_value):
+        pred_words.append(idx2label[pred_idx])
+        pred_probs.append(pred_value)
 
-            text_split = text.split(" ")
-            for i, word in enumerate(text_split):
-                if word.isupper() and not has_numbers(word):
-                    ab_locs = torch.from_numpy(
-                        np.array([i])
-                    )
+    print(f"Top 5 preidction of abbreviation: {word}")
+    for i in range(len(pred_words)):
+        print(pred_words[i], pred_probs[i]) 
 
-                    logits = model(sents_idx, ab_locs)
+
+    # for data_idx in test_dl:
+    #     if data_idx.item() == 1:
+    #         text = test['TEXT'][data_idx.item()]
+    #         sents_idx, ab_locs, labels = test_data[data_idx]
+
+    #         print("Text:", text)
+    #         print("="*50)    
+
+    #         text_split = text.split(" ")
+    #         for i, word in enumerate(text_split):
+    #             if word.isupper() and not has_numbers(word):
+    #                 ab_locs = torch.from_numpy(
+    #                     np.array([i])
+    #                 )
+
+    #                 print(ab_locs.size())
+    #                 print(sents_idx)
+    #                 logits = model(sents_idx, ab_locs)
                     
-                    prob = softmax(logits)
-                    pred_5_value, pred_5_idx = torch.topk(prob, k=5, dim=1)
-                    pred_words, pred_probs = [], []
-                    pred_5_idx, pred_5_value =  pred_5_idx.cpu().detach().numpy()[0], pred_5_value.cpu().detach().numpy()[0]
-                    for pred_idx, pred_value in zip(pred_5_idx, pred_5_value):
-                        pred_words.append(idx2label[pred_idx])
-                        pred_probs.append(pred_value)
+    #                 prob = softmax(logits)
+    #                 pred_5_value, pred_5_idx = torch.topk(prob, k=5, dim=1)
+    #                 pred_words, pred_probs = [], []
+    #                 pred_5_idx, pred_5_value =  pred_5_idx.cpu().detach().numpy()[0], pred_5_value.cpu().detach().numpy()[0]
+    #                 for pred_idx, pred_value in zip(pred_5_idx, pred_5_value):
+    #                     pred_words.append(idx2label[pred_idx])
+    #                     pred_probs.append(pred_value)
 
-                    print(f"Top 5 preidction of abbreviation: {word}")
-                    for i in range(len(pred_words)):
-                        print(pred_words[i], pred_probs[i])
+    #                 print(f"Top 5 preidction of abbreviation: {word}")
+    #                 for i in range(len(pred_words)):
+    #                     print(pred_words[i], pred_probs[i])
 
-                    print("="*50)
+    #                 print("="*50)
             
-            return 
+    #                 return 
     
 
 if __name__ == '__main__':
